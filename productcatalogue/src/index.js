a7432fc534d0257b8e645b2ccdf073453e08737a
Liv Sarah <sarahansnes@gmail.com>
2020-06-09 00:46:02 +0200

    Added new empty components

--- productcatalogue/src/index.js
+++ productcatalogue/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import AddProduct from './Components/AddProduct';
+import EditProduct from './Components/EditProduct';
+import ShowProduct from './Components/ShowProduct';
+import {BrowserRouter, Route} from 'react-router-dom'; 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render(<BrowserRouter>
+<Route exact path="/" component = {App}></Route>
+<Route path="/create" component = {AddProduct}></Route>
+<Route path="/show/:id" component = {EditProduct}></Route>
+<Route path="/edit/:id" component = {ShowProduct}></Route>
+</BrowserRouter>, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
