a8e2f8b2427d0a192729ec69f733244865ff8571
Liv Sarah <sarahansnes@gmail.com>
2020-06-09 12:33:00 +0200

    Added empty card and style

--- productcatalogue/src/App.js
+++ productcatalogue/src/App.js
@@ -2,15 +2,34 @@ import React from 'react';
 import 'App.css';
 import firebase from '../Config';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card} from 'react-bootstrap';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
+    const cardStyles = {
+      width: 'auto',
+      height: 'auto',
+      backgroundColor: 'white',
+      margin: 'auto', 
+      display: 'block', 
+      marginTop: '60px', 
+      opacity: 0.5, 
+      paddingTop: '10px', 
+      paddingLeft: '20px', 
+      paddingRight: '20px', 
+      borderStyle: 'outset', 
+      borderLeft: '50px solid black', 
+      borderRadius: '20px'
+
+    }
     return( 
       <div>
-        
+        <Card>
+
+        </Card>
       </div>
     )
   }
