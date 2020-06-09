fcea616e37a05c3b0e226eb1d0eb9c7688c51ba0
Liv Sarah <sarahansnes@gmail.com>
2020-06-09 14:46:57 +0200

    Added content to main page, loading data from firebase

--- productcatalogue/src/App.js
+++ productcatalogue/src/App.js
@@ -1,13 +1,41 @@
 import React from 'react';
-import 'App.css';
-import firebase from '../Config';
+import './App.css'
+import firebase from './Config';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Card} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.ref = firebase.firestore().collection("Products");
+    this.unsubscribe = null;
+    this.state = {
+      products : []
+    };
   }
+
+  componentDidMount() {
+    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const products = [];
+    querySnapshot.forEach((doc) => {
+      const {name, description, url} = doc.data();
+      products.push({
+        key: doc.id, 
+        doc, 
+        name, 
+        description, 
+        url
+      });
+    });
+    this.setState({
+      products
+    });
+  }
+
   render() {
     const cardStyles = {
       width: 'auto',
@@ -16,7 +44,7 @@ class App extends React.Component {
       margin: 'auto', 
       display: 'block', 
       marginTop: '60px', 
-      opacity: 0.5, 
+      opacity: 0.8, 
       paddingTop: '10px', 
       paddingLeft: '20px', 
       paddingRight: '20px', 
@@ -27,8 +55,39 @@ class App extends React.Component {
     }
     return( 
       <div>
-        <Card>
+        <Card style={cardStyles}>
+          <div className="button">
+            <Link to="/create">
+              <button class="add-btn">Add product</button>
+            </Link>
+          </div>
 
+          <div class="container">
+            <div class="panel panel-heading">
+              <h3 class="panel heading">Product Details</h3>
+            </div>
+          </div>
+
+          <div class="panel-body">
+            <table class="table table-stripe">
+              <thead>
+                <tr>
+                  <th>Product name</th>
+                  <th>Description</th>
+                  <th>Image</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.products.map(product =>
+                  <tr>
+                    <td><Link to={`/show/${product.key}`}>{product.name}</Link></td>
+                    <td>{product.description}</td>
+                    <td><img src={product.url} style={{maxWidth: 150, maxHeight: 150}} alt=""></img></td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
         </Card>
       </div>
     )
