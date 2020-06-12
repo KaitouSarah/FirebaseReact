3a08a9d5e80112f7e7810b550a57434159cf4eca
Liv Sarah <sarahansnes@gmail.com>
2020-06-12 07:45:16 +0200

    Created basic class structure

---
+++ productcatalogue/src/Components/ShowProduct.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../App.css';
+import firebase from '../Config';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Card} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
+
+class ShowProduct extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <Card style={}>
+                    
+                </Card>
+            </div>
+        )
+    }
+}
+export default ShowProduct;
